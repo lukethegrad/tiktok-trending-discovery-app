ad70ddb43d2efcc076051ed02ad95f82672161f0
lukethegrad <luke@thegraduation.co>
2025-06-30 17:11:51 +0100

    Update app.py - added the updates about cleaning data (init)

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 
 from apify_utils import run_trending_scraper
+from data_utils import process_raw_data
 
 
 st.set_page_config(page_title="TikTok Trending Discovery", layout="wide")
@@ -13,11 +14,12 @@ st.markdown("This tool pulls the top 100 trending TikTok sounds via Apify.")
 # Run button
 if st.button("Fetch Trending Songs"):
     with st.spinner("Fetching data from Apify..."):
-        st.write("API key loaded:", os.getenv("APIFY_API_KEY"))
         df = run_trending_scraper()
         if df is not None and not df.empty:
-            st.success(f"Fetched {len(df)} songs!")
-            st.dataframe(df)
+            clean_df = process_raw_data(df)
+            st.success(f"Fetched {len(clean_df)} clean songs!")
+            st.dataframe(clean_df)
+
 
             # Download buttons
             csv = df.to_csv(index=False).encode("utf-8")
